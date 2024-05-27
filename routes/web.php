b8f1fcbb6b6a642e31e3a22e3181c6adf39e0984
tatsu30 <desutokai@gmail.com>
2024-05-27 19:39:16 +0900

    再修正

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,12 @@ Route::post('login', [AuthenticatedSessionController::class, 'store']);
 Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::resource('products', ProductController::class);
+    
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 });
